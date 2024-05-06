0e1408953458bfadfae21beda0edf562dd655640
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-05-06 23:23:24 +0300

    DTOs

---
+++ OnionPortfolioProject.Application/DTOs/ArticleDto.cs
@@ -0,0 +1,8 @@
+namespace OnionProtfolioProject.Application.DTOs;
+
+public class ArticleDto
+{
+    public Guid Id { get; set; }
+    public string Header { get; set; }
+    public string Content { get; set; }
+}
---
+++ OnionPortfolioProject.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,6 @@
+namespace OnionProtfolioProject.Application.DTOs;
+
+public class CategoryDto
+{
+    public Guid CategoryId { get; set; }
+    public string CategoryName { get; set; } }
