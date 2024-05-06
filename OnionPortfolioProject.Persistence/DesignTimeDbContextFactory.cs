42dceb0a390d56b9ecd6285bbbc99f8d19367f1d
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-05-06 23:26:22 +0300

    DesignTimeDbContextFactory Created

--- OnionPortfolioProject.Persistence/DesignTimeDbContextFactory.cs
+++ OnionPortfolioProject.Persistence/DesignTimeDbContextFactory.cs
@@ -9,7 +9,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataDbCont
     public DataDbContext CreateDbContext(string[] args)
     {
         DbContextOptionsBuilder<DataDbContext> dbContextOptionsBuilder = new();
-        dbContextOptionsBuilder.UseSqlServer("Server=localhost, 1433;Database=PortfolioProject;User Id=SA;Password=reallyStrongPwd123;TrustServerCertificate=True;");
+        dbContextOptionsBuilder.UseSqlServer("Server=localhost, 1433;Database=OnionArcPortfolioProject;User Id=SA;Password=reallyStrongPwd123;TrustServerCertificate=True;");
         return new DataDbContext(dbContextOptionsBuilder.Options);
     }
 }
