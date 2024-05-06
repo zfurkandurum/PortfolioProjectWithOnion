5e4b1a87369c630aad3e7d510a4fc4735cb3f81a
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-05-06 23:28:59 +0300

    Mapping configuration in Program.cs

--- OnionPortfolioProject.API/Program.cs
+++ OnionPortfolioProject.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using OnionPortfolioProject.API.Controller;
 using OnionProtfolioProject.Application.Interfaces.Repositories;
+using OnionProtfolioProject.Application.Mapping;
 using OnionProtfolioProject.Persistence.Repositories;
 using PortfolioProjectOnion.Persistence.Context;
 
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
 builder.Services.AddSwaggerGen();
